a073439386e274e319abec10c21615a9a33ef623
JannisBoth <jannisboth@gmail.com>
2021-02-25 14:48:07 +0000

    Generalized code and run model

--- features/ngram_model.py
+++ features/ngram_model.py
@@ -2,17 +2,26 @@ from collections import Counter, defaultdict
 import numpy as np
 import dill
 import os
+import re
+import time
 
-'''
-# Set path as needed for Query_Completer class
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)'''
+
 
 class Query_Completer():
     def __init__(self, n):
         self.n = n
         self.model = defaultdict(lambda: defaultdict(lambda: 0))
+
+    def preprocess_lyrics(self, lyrics):
+        """
+        Function which returns the tokenized and lowered lyrics
+
+        :param lyrics: The lyrics to preprocess (str)
+        :return line: Tokenized and lowered list of tokens (list)
+        """
+        tokenized = re.findall(r"[\w]+", lyrics)
+        line = [x.lower() for x in tokenized if x != ""]
+        return line
 
 
     def create_ngram(self, sentence):
@@ -48,44 +57,51 @@ class Query_Completer():
         return ngram_list
 
 
-    def add_sentence(self, sentence):
+    def add_single_lyric(self, lyrics):
         """
         Function which adds the ngrams of the given sentence to the model
 
-        :param sentence: Tokens (list)
+        :param lyrics: The lyrics (str)
         """
-
-        ngram_list = self.create_ngram(sentence)
+        token_list = self.preprocess_lyrics(lyrics)
+        ngram_list = self.create_ngram(token_list)
 
         for tokens in ngram_list:
             # The last token in the context of the former tokens is more likely through this example
             self.model[tokens[:-1]][tokens[-1]] += 1
 
 
-    def add_sentences(self, sentence_list):
+    def add_lyrics(self, lyric_list):
         """
         Function which adds the ngrams of the given sentences to the model
 
-        :param sentence_list: List of token lists (list)
+        :param lyric_list: List of lyrics (list of strings)
         """
-        for sentence in sentence_list:
-            self.add_sentence(sentence)
+        for lyrics in lyric_list:
+            self.add_single_lyric(lyrics)
 
 
-    def predict_next_token(self, token_list):
+    def predict_next_token(self, current_query):
         """
         Function which returns most probable next words based on the model
 
-        :param token_list: Tokens (list)
-        :return sorted_result: Sorted list of most probable continuations (list)
+        :param current_query: The currently inserted query (str)
+
+        :return sorted_result: Sorted list of most probable continuations + sentence (list of str)
         """
+        
+        query_token_list = self.preprocess_lyrics(current_query)
+
+        if len(query_token_list) < self.n-1:
+            # If not enough tokens in query
+            return None
 
         # extracts the last m = n-1 tokens from the token list
-        last_m_tokens = token_list[-(self.n-1):]
+        last_m_tokens = query_token_list[-(self.n-1):]
         results = dict(self.model[tuple(last_m_tokens)]) # returns the relevant dict of the model
         
         # Sorts the keys by the value and returns them with the most probable word in the first position
-        sorted_result = [k for k, v in sorted(results.items(), key=lambda item: item[1],reverse = True)]        
+        sorted_result = [current_query + " " + word for word, v in sorted(results.items(), key=lambda item: item[1],reverse = True)[0:5]]        
         return sorted_result
 
 
@@ -109,16 +125,39 @@ class Query_Completer():
         with open(filepath, 'rb') as file:
             self.model = dill.load(file)
 
+
+# Set path as needed for Query_Completer class
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
 '''
-with open("processed_lyrics.txt", "r") as f:
-    lines = f.readlines()
-    
-lines = [line.split() for line in lines]
-
-
 qc = Query_Completer(n = 3)
-#qc.add_sentences(lines[0:2000])
 
-#qc.save_model("qc_model.pkl")
+# Train on current data
+import pandas as pd
+
+begin = time.time()
+qc = Query_Completer(n = 3)
+data = pd.read_csv("data-song-sample.csv")
+
+for idx, row in data.iterrows():
+    qc.add_single_lyric(row["SongLyrics"])
+
+qc.save_model("qc_model.pkl")
+print(f"Training and saving took: {time.time() - begin}")
+
+
+# Reload the model and make predictions
 qc.load_model("qc_model.pkl")
-print(qc.predict_next_token(["night", "gotta"]))'''
+print("Predicting")
+print(qc.predict_next_token("deux trois"))
+print(qc.predict_next_token("Cinq six sept"))
+print(qc.predict_next_token("did it"))
+print(qc.predict_next_token("Oops I"))
+print(qc.predict_next_token("My loneliness"))
+print(qc.predict_next_token("Es ragen aus ihrem aufgeschlitzten Bauch"))'''
+
+
+
